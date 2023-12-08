56ea893a11425bdcc1e8c774fcc018f877e30579
Maua Santiago <136342770+Maua488@users.noreply.github.com>
2023-12-07 22:50:50 -0300

    atividade 3

---
+++ SEMANA 1/ATIVIDADE 3/script.js
@@ -0,0 +1,36 @@
+function changeStyle() {
+    const divs = document.querySelectorAll(".block");
+
+    for(let i = 0;i < divs.length; i++){
+        const div = divs[i];
+        div.classList.toggle("circle")
+    }
+
+    const listItems = document.querySelectorAll("li");
+
+    for (let i = 0; i < listItems.length; i++){
+        const li = listItems[i];
+        li.classList.toggle("itemCard");
+    }
+
+    const spans = document.querySelectorAll("span");
+    const paragraphs = document.querySelectorAll("p");
+
+    for (let i = 0; i < spans.length; i++){
+        const span = spans[i];
+        const p = paragraphs[i];
+
+        const online = span.classList.toggle("online");
+
+        if(online){
+            span.classList.remove("offline");
+            p.innerText = "Online";
+        }  else {
+            span.classList.add("offline");
+            p.innerText = "Offline";
+        }  
+    }
+
+}
+
+changeStyle()
